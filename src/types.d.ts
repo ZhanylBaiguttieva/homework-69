6dcdf0f7bdbd73a2708362fff40dca5a6b9ab632
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2024-01-05 06:10:24 +0600

    Added type Movie for one Movie

--- src/types.d.ts
+++ src/types.d.ts
@@ -7,3 +7,15 @@ export interface ApiSearchMovies {
     score: number;
     show: SearchMovies;
 }
+
+export interface Movie {
+    id: number;
+    name: string;
+    language: string;
+    genres: string [];
+    premiered: string;
+    image: {
+        medium: string;
+    } |  null;
+    summary: string;
+}
